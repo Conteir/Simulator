99ab7109ff83861dd8be066c851a924221038afb
Hanna <igerne94@gmail.com>
2021-07-15 11:45:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { FindID } from '../components/pages/FindID.jsx';
+import { SearchInHAPI } from '../components/pages/SearchInHAPI.jsx';
+import { Documentation } from '../components/pages/Documentation.jsx';
+import { EPJSimulator } from '../components/pages/EPJSimulator.jsx';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export const App = class App extends React.Component {
+  
+  
+  render() {
+
+    return (
+      <div className="App">
+
+        <Router>
+          <div>
+            {/* Set defaultActiveKey to the current 'rest' path in order to
+              switch the Nav tab to the current selected page */}
+            <Nav variant="tabs" defaultActiveKey={window.location.pathname}>
+
+                <Nav.Link href="/epjsimulator">EPJ-simulator</Nav.Link>
+
+                <Nav.Link href="/">Søk i HAPI</Nav.Link>
+              
+                <Nav.Link href="/finnid">Finn ID</Nav.Link>
+             
+                <Nav.Link href="/dokumentasjon">Dokumentasjon</Nav.Link>
+            
+            </Nav>
+
+            <Switch>
+
+                <Route path="/epjsimulator" component={EPJSimulator}>
+                  <EPJSimulator />
+                </Route>
+
+                <Route exact path="/" >
+                  <SearchInHAPI/>
+                </Route>
+
+                <Route path="/finnid" component={FindID}>
+                  <FindID />
+                </Route>
+
+                <Route path="/dokumentasjon" component={Documentation}>
+                  <Documentation />
+                </Route>
+
+                
+
+            </Switch>
+
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/pages/Documentation.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../pages/../../index.css';
+
+export const Documentation = class Documentation extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="jumbotron text-center">
+                    <h1>Dokumentasjon</h1>
+                    <p>Read this important information</p>
+                </div>
+               
+                <p>Søk i HAPI er en enkel tjeneste for å gjøre oppslag mot Helsedirektoratets
+                    innholdstjenester i API-et HAPI. Dokumentasjon og tilgang til API-et
+                    finner du her: 
+                        <a href="https://utvikler.helsedirektoratet.no">https://utvikler.helsedirektoratet.no</a>
+                </p>
+
+                <h2>Slik bruker du tjenesten</h2>
+                <p>Du kan gjøre spørringer etter alt innhold i API-et basert på enten:</p>
+                    <ul>
+                        <li>Innholdets unike ID</li>
+                        <li>En kode fra et kodeverk eller en terminologi. Du vil da få oppslag på innhold som er <em>klinisk relevant</em>
+                            for den koden eller begrepet du bruker i søket.</li>
+                    </ul>
+                <h3>Slik finner du unik ID til innhold</h3>
+                <p>Under menypunktet «Get ID» velger du innholdstypen du ønsker å hente ut av API-et.
+                    Du trykker deretter på «Search» og tjenesten vil returnere alle instanser av denne innholdstypen med
+                    tittel og ID. Du kan deretter kopiere ID-en og lime den inn i søkefeltet «HAPI-id» på forsiden.
+                </p>
+                <h2>Naviger til tilknyttet innhold</h2>
+                <p>Under fanen "Link navigation" finner du lenker til tilknyttet innhold. Tilknyttet innhold kan enten være foreldre eller barn. Et eksempel:</p>
+                <p>En <em>retningslinje</em> vil ha <em>kapitler</em> som <em>barn</em>. <em>Kapitler</em> vil igjen ha <em>anbefalinger</em> som barn.
+                    Hvis du henter ut en <em>anbefaling</em> vil <em>kapittelet</em> det hører til listes ut som en forelder.</p>
+            </div>
+        )
+    }
+
+}
+
+export default Documentation;
